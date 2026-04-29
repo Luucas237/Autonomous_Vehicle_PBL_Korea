7f8e5d85a6af8631cbef4dd6f16a68a4762f7f8c
juliavx <147629468+juliavx@users.noreply.github.com>
2026-04-29 12:51:26 +0200

    Update robot_test.py

--- src/mentorpi_vision/mentorpi_vision/robot_test.py
+++ src/mentorpi_vision/mentorpi_vision/robot_test.py
@@ -1,43 +1,32 @@
-import pathlib
-import platform
 import torch
 import cv2
-
-current_system = platform.system()
-if current_system == 'Windows': 
-    pathlib.PosixPath = pathlib.WindowsPath
-
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='best14.pt')
-
-model.conf = 0.30  #odczytuje z wieksza pewnoscia
-model.iou = 0.45 
-model.multi_label = False 
-
+import pathlib
 import sys
-camera_id = int(sys.argv[1]) if len(sys.argv) > 1 else 0 #pobiera kamere urzadzenia
-cap = cv2.VideoCapture(camera_id)
 
-if not cap.isOpened():
-    print(f"BŁĄD: Nie można otworzyć kamery o ID: {camera_id}")
-    exit()
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
 
-print(f"Klasy modelu: {model.names}")
-print("Naciśnij 'q', aby zamknąć okno.")
+path = 'best14.pt'
+model = torch.hub.load('ultralytics/yolov5', 'custom', path=path, force_reload=True)
 
-while True:
+cam_id = int(sys.argv[1]) if len(sys.argv) > 1 else 0
+cap = cv2.VideoCapture(cam_id)
+
+print(f"Uruchamiam detekcję na kamerze ID: {cam_id}")
+
+while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
-        print("Błąd: Nie można pobrać klatki.")
+        print("Błąd: Nie można odebrać klatki z kamery.")
         break
-
+        
     results = model(frame)
-
-    annotated_frame = results.render()[0]
-
-
-    cv2.imshow('Wizja Projektu Julii', annotated_frame)
-
-   
+    results.conf = 0.30
+    
+    rendered_frame = results.render()[0]
+    
+    cv2.imshow('Detekcja Znakow - MentorPi', rendered_frame)
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
