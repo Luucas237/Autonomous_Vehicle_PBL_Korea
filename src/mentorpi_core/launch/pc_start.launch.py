4a5daa75e292594b5ab08938e8e6f2c9000ec1c3
Łukasz Grabarski <118926223+Luucas237@users.noreply.github.com>
2026-05-15 15:24:09 +0200

    Fix formatting in pc_start.launch.py

--- src/mentorpi_core/launch/pc_start.launch.py
+++ src/mentorpi_core/launch/pc_start.launch.py
@@ -4,7 +4,7 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    
+     
     vision_pc_node = Node(
         package='mentorpi_vision',
         executable='lane_detector',
